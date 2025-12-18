0501a836726c4e2aa5c7ad5d073fa4fe6d35f161
santhoshkumarCodes <santhoshkumartechsavvy@gmail.com>
2025-12-18 15:12:52 +0530

    web: initialize Next.js project.
    Add Tailwind CSS, TypeScript, ESLint, and Redux Toolkit for the web next.js project.

---
+++ web/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NextDesk",
+  description: "An open-source, browser-based collaborative coding workspace with built-in video calling",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
