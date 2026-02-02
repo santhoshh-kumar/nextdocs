bf3b2f4e79ed91e5ba29d508825c8bd1b7d59a4a
Santhosh Kumar <santhoshkumartechsavvy@gmail.com>
2026-02-02 19:07:02 +0530

    Revert "web: Introduce realtime code editor."

--- web/jest.config.js
+++ web/jest.config.js
@@ -8,9 +8,6 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jest-environment-jsdom",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-  },
 };
 
 module.exports = createJestConfig(customJestConfig);
